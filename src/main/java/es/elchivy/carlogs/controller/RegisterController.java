27f02cf814834fb60af29691a00df0ee397bcfcf
scasaf00 <scasaf00@estudiantes.unileon.es>
2022-05-08 19:38:45 +0200

    Sin miedo al exito II registerController

--- src/main/java/es/elchivy/carlogs/controller/RegisterController.java
+++ src/main/java/es/elchivy/carlogs/controller/RegisterController.java
@@ -1,4 +1,47 @@
 package es.elchivy.carlogs.controller;
 
-public class RegisterController {
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.view.ViewScoped;
+import java.io.Serializable;
+
+@ManagedBean(name = "registerController")
+@ViewScoped
+public class RegisterController implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @ManagedProperty("#{registerController.uname}")
+    private String uname;
+
+    @ManagedProperty("#{registerController.password}")
+    private String password;
+
+
+    @ManagedProperty("#{registerController.userType}")
+    private String userType;
+
+    public String getUname() {
+        return this.uname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUserType() {
+        return this.userType;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
 }
