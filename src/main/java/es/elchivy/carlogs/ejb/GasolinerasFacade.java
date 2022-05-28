0a1db80e016d81fb54057c0996e84b59a6ae653b
scasaf00 <scasaf00@estudiantes.unileon.es>
2022-05-28 19:20:15 +0200

    Facade nuevos

---
+++ src/main/java/es/elchivy/carlogs/ejb/GasolinerasFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package es.elchivy.carlogs.ejb;
+
+import es.elchivy.carlogs.modelo.Gasolineras;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author scfmm
+ */
+@Stateless
+public class GasolinerasFacade extends AbstractFacade<Gasolineras> implements GasolinerasFacadeLocal {
+
+    @PersistenceContext(unitName = "CarLogsPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public GasolinerasFacade() {
+        super(Gasolineras.class);
+    }
+    
+}
